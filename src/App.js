e70e468f877c5675c759f1b0f268ac15814fb1bc
BlertaPanduri <blertabajri@gmail.com>
2020-08-25 16:01:36 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Route,NavLink, Switch } from 'react-router-dom';
+import Users from './containers/Users/Users.js';
+import Courses from './containers/Courses/Courses.js';
+import Course from './containers/Course/Course.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul style={{ listStyle:"none"}}>
+          <li style={{ display:"inline-block", margin:"10px"}}><NavLink to = "/courses">Courses</NavLink></li>
+          <li style={{ display:"inline-block", margin:"10px"}}><NavLink to = "/users">Users</NavLink> </li>
+        </ul>
+      </nav>
+      
+      <Route path = "/users" component = {Users}/>
+      <Route path = "/courses" exact component = {Courses}/>
+      <Route path = "/courses/course" component = {Course}/>
+    
+     
     </div>
   );
 }
